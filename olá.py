8e86a88eb8f67197f0ac9478c7d3400e798b70aa
Sandy <sandyney3@gmail.com>
2019-12-14 16:18:11 -0300

    My Django Girls app, first commit

---
+++ olá.py
@@ -0,0 +1,6 @@
+idade=12
+if idade >= 18:
+    print('pode entrar')
+else:
+    print('não autorizado')
+    
---
+++ teste.py
@@ -0,0 +1,11 @@
+nome= 'Sandy'
+def hi(nome):
+    if nome == 'Sandy':
+        print ('Olá Sandy')
+        print ('Boa tarde')
+    else :
+        print('Olá')
+        print('Tudo bem?')
+        print('Boa tarde')
+hi('Maria')
+
